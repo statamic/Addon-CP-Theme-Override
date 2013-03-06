ecf1395ad2864b835cf806dbb7990a39ce745632
Rommert <rommert@beeldspraak.com>
2013-03-06 10:28:22 +0100

    Update hooks.override.php

--- hooks.override.php
+++ hooks.override.php
@@ -1,8 +1,16 @@
 <?php
 class Hooks_override extends Hooks {
 
+  //version < 1.5
   function add_to_control_panel_head() {
     return self::include_css('override.css');
   }
+  
+  
+  //version 1.5 and above
+  function control_panel__add_to_head() {
+    return $this->css->link('override.css');
+  }
+  
 
 }
